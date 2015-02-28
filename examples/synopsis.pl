cc210992ee6b719e6a59470c1167989a888b537b
Tom Molesworth <tom@perlsite.co.uk>
2015-02-28 17:55:37 +0000

    Synopsis example
    git-svn-id: http://entitymodel.com/svn/trunk/Tickit-Widget-LogAny@2993 c5bf8c3b-bf20-4ed3-b413-d5547a49f493

---
+++ examples/synopsis.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+use Tickit;
+use Tickit::Widget::LogAny;
+use Log::Any qw($log);
+
+my $tickit = Tickit->new(
+	root => Tickit::Widget::LogAny->new(
+		stderr => 1,
+	)
+);
+print STDERR "print to STDERR\n";
+printf STDERR "printf(...) to %s", 'STDERR';
+$log->trace('trace message');
+$log->info('info message');
+$log->debug('debug message');
+$log->notice('notice message');
+$log->warn('warn message');
+$log->error('error message');
+$log->critical('critical message');
+$tickit->run;
